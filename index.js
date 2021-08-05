5f0dc5ce58e8f757d5117854e056b41828089fca
Roberto <roberto.cmp16@gmail.com>
2021-08-05 09:13:25 -0400

    More options added, Rock vs others options added

--- index.js
+++ index.js
@@ -11,22 +11,37 @@ const choice = (p1, p2) => {
         // SCISSORS
         case `${scissors}-${paper}`:
         case `${scissors}-${lizard}`:
+        case `${paper}-${scissors}`:
+        case `${lizard}-${scissors}`:
             console.log(`${scissors}` + ' wins');
             break;
         // PAPER 
         case `${paper}-${rock}`:
         case `${paper}-${spock}`:
+        case `${rock}-${paper}`:
+        case `${spock}-${paper}`:
             console.log(`${paper}` + ' wins');
             break;
         // LIZARD 
         case `${lizard}-${spock}`:
         case `${lizard}-${paper}`:
+        case `${spock}-${lizard}`:
+        case `${paper}-${lizard}`:
             console.log(`${lizard}` + ' wins');
             break;
         // SPOCK
         case `${spock}-${scissors}`:
         case `${spock}-${rock}`:
+        case `${rock}-${spock}`:
+        case `${scissors}-${spock}`:
             console.log(`${spock}` + ' wins');
+            break;
+        // ROCK
+        case `${rock}-${lizard}`:
+        case `${rock}-${scissors}`:
+        case `${scissors}-${rock}`:
+        case `${lizard}-${rock}`:
+            console.log(`${rock}` + ' wins');
             break;
         default:
             console.log('tie');
